7f141920ec8b30d43942c2542181803ff94bf599
Uday Vaddina <UdayVaddina@users.noreply.github.com>
2016-11-27 14:46:57 -0600

    Plot 1
    Further to addition of a Master file, making changes only to include the plot code after sourcing the Master file.

--- Plot1_ExD1.R
+++ Plot1_ExD1.R
@@ -1,26 +1,4 @@
-rm(list = ls(all = TRUE))
-
-# Download the dataset
-fileurl='https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
-if (!file.exists('./data/household_power_consumption.zip')){
-  dir.create("./data")
-  download.file(fileurl, './data/household_power_consumption.zip', mode = 'wb')
-}
-
-# unzip dataset into the folder data
-data_file= unzip(zipfile="./data/household_power_consumption.zip",exdir="./data")
-
-hPowCons <- read.table( data_file, header = TRUE, sep = ';', na.strings = '?', dec = "." )
-twoDaysData <- hPowCons[hPowCons$Date %in% c("1/2/2007","2/2/2007") ,]
-#str(twoDaysData)
-
-dayTimeTmp <- paste(twoDaysData$Date, twoDaysData$Time, sep = " ")
-#class(dayTimeTmp)
-
-dayTimeData <- strptime(dayTimeTmp, "%d/%m/%Y %H:%M:%S")
-#class(dayTimeData)
-
-globalActivePower <- as.numeric(twoDaysData$Global_active_power)
+source("Master.R")
 png("plot1.png", width=480, height=480)
 
 hist(globalActivePower, 
